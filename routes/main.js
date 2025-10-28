62635683f3aa9e2c9f20ea7e8622129918eeef68
cejeh001 <cejeh001@gold.ac.uk>
2025-10-28 17:36:49 +0000

    `Added search functionality with form in search.ejs and corresponding route in main.js`

--- routes/main.js
+++ routes/main.js
@@ -8,10 +8,13 @@ var shopData = {shopName: "The Thirsty Student",
             };
 
 // handle main routes
-// TODO
+
 router.get("/", (req, res) => {res.render("index.ejs", shopData)});
 router.get("/about", (req, res) => {res.render("about.ejs")});
 router.get("/search", (req, res) => {res.render("search.ejs")});
+
+// TODO: search in the database based on req.query.search_text and req.query.category
+router.get("/search-result", (req, res) => { res.send("You searched for " + req.query.search_text + " in " + req.query.category);});
 
 // export the router object so index.js can access it
 module.exports = router;
