bbd4d3b186deee4742fba0dc8e9dae80c6330e20
cejeh001 <cejeh001@gold.ac.uk>
2025-10-28 11:54:54 +0000

    dynamically edited the index ejs file

---
+++ routes/README.md
@@ -0,0 +1,3 @@
+#Thisrty Student App
+
+This is a simple app that uses EJS to be more accustomed to each user.
--- routes/main.js
+++ routes/main.js
@@ -5,7 +5,8 @@ const router = express.Router();
 // handle main routes
 // TODO
 router.get("/", (req, res) => {res.render("index.ejs")});
-router.get("/", (req, res) => {res.render("about.ejs")});
+router.get("/about", (req, res) => {res.render("about.ejs")});
+router.get("/search", (req, res) => {res.render("search.ejs")});
 
 // export the router object so index.js can access it
 module.exports = router;
